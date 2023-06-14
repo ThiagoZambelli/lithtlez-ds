45a166ce6767604851261cea5338f5459fb0274f
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-06-14 12:27:07 -0300

    fix: implementando className no card

--- src/componentes/LzCard/index.tsx
+++ src/componentes/LzCard/index.tsx
@@ -4,19 +4,21 @@ import styled from 'styled-components';
 export interface LzCardProps {
     children: ReactNode,
     bgColor?: string,
+    className?: string
 }
 
 const CardEstilizado = styled.section<LzCardProps>`
     padding: 48px;
     background: ${(props: LzCardProps) => props.bgColor
-    ? props.bgColor : '#FFFFFF'};
+        ? props.bgColor : '#FFFFFF'};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;   
+    border-radius: 10px;
+    ${props => props.className ? props.className : ''} 
 `
 
-export const LzCard = ({children, bgColor} : LzCardProps) => {
+export const LzCard = ({ children, bgColor, className }: LzCardProps) => {
     return (
-        <CardEstilizado bgColor={bgColor}>
+        <CardEstilizado className={className} bgColor={bgColor}>
             {children}
         </CardEstilizado>
     )
