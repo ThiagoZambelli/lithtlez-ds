b0773def2a108552e18797eb6d55cf52650a8048
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-06-19 12:32:22 -0300

    Correção de type input

--- src/componentes/LzInput/index.tsx
+++ src/componentes/LzInput/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export interface LzInputProps extends ICores {
     label: string;
-    type?: 'text' | 'email' | 'password' | 'date';
+    type?: 'text' | 'email' | 'password' | 'date'| 'number';
     onChange: (value: string) => void;
     value: string;
 }
